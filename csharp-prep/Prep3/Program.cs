5c883ed1d6a48f68a23a7c9d5e038ec33b37b97f
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-05-07 23:39:04 -0600

    12

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Person person = new Person();
+        person._givenName = "Oscar";
+        person._familyName = "Rodriguez";
+        person.ShowWesternName();
+        person.ShowEasternName();
+        person._givenName = "Mercedes";
+        person._familyName = "Ramirez";
+        person.ShowEasternName();
+        person.ShowWesternName();
+
+        Blind kitchen = new Blind();
+        kitchen._width = 60;
+        kitchen._height = 48;
+        kitchen._color = "white";
+        Console.WriteLine($"The blind for this kitchen is: {kitchen._color}");
+        double materialAmount = kitchen.GetArea();
+        Console.WriteLine($"The total material to use is: {materialAmount} sq.in.");
+    }
+
+    public class Person
+    {
+        public string _givenName = "";
+        public string _familyName = "";
+
+        public Person()
+        {
+
+        }
+        public void ShowEasternName()
+        {
+            Console.WriteLine($"{_familyName}, {_givenName}");
+        }
+        public void ShowWesternName()
+        {
+            Console.WriteLine($"{_givenName} {_familyName}");
+        }
+    }
+    public class Blind
+    {
+        public float _width;
+        public float _height;
+        public string _color;
+        public double GetArea()
+        {
+            return _width * _height;
+        }
+
     }
 }
