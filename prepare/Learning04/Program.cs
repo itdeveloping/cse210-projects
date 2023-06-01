125d3f47a18ac4d901b4c08c9ded7e3d3596bc23
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-05-31 23:08:48 -0600

    12

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Inheritance Learning Activity
+
+        Console.Clear();
+        //super class
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine("Super class: Assignment");
+        Console.WriteLine(assignment1.GetSummary());
+        //subclass
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine("\nSubclass: MathAssignment");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        //subclass
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+
+        Console.WriteLine("\nSubclass: MathAssignment");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
+
     }
 }
